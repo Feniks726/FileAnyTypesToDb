22789af935fcdb141d8b7ef3807346a22e5b1397
Feniks726 <chalmov@yandex.ru>
2022-04-03 13:28:58 +0300

    Context

---
+++ source/FileAnyTypesToDb.Context/ContextFactories/MasterDataContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using System.Data.Entity.Infrastructure;
+
+namespace OrdersProject.Context.ContextFactories
+{
+    public class MasterDataContextFactory : IDbContextFactory<MasterDataContext>
+    {
+        public MasterDataContext Create()
+        {
+            var dbContext = new MasterDataContext();
+
+            dbContext.Database.CommandTimeout=int.Parse(ConfigurationManager.AppSettings["SQLCommandTimeOut"]);
+
+            return dbContext;
+        }
+    }
+}
---
+++ source/FileAnyTypesToDb.Context/MasterDataContext2.cs
@@ -0,0 +1,13 @@
+﻿namespace FileAnyTypesToDb.Context
+{
+    public class MasterDataContext2 : DbContext
+    {
+        public MasterDataContext2() : base("name=MasterDataContext")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
