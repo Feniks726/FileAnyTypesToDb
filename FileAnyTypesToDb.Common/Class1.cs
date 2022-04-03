88b7ee416aa1264ec7b4195e9db9a14d77af50ce
Feniks726 <chalmov@yandex.ru>
2022-04-03 12:25:00 +0300

    Initial

---
+++ FileAnyTypesToDb.Common/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAnyTypesToDb.Common
+{
+    public class Class1
+    {
+    }
+}
